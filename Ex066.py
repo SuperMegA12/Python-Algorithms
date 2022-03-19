663f1f0ef72e0bb8f591771b56e07ff3acefd567
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-19 01:30:22 -0300

    "first commit"

---
+++ Ex066.py
@@ -0,0 +1,15 @@
+cont_1 = 0
+cont_2 = 0
+soma = 0
+n = int(input('Digite um número:'))
+soma+=n
+cont_1+=1
+while n !=999:
+    n = int(input('Digite outro número: '))
+    cont_2+=1
+    soma += n
+    if n == 999:
+        cont_2-=1
+        soma-=n
+        break
+print(f'Você digitou {cont_1+cont_2} valores. E a soma deles corresponde a {soma}')
