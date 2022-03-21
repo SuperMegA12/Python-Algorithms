42d959fb9e663017de9ede1d29f63132cdcbc8b2
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-21 16:58:45 -0300

    Estatísticas em Produto

---
+++ Ex070.py
@@ -0,0 +1,19 @@
+list = []
+cont = soma  = 0
+barato =''
+while True:
+    nome = str(input('Digite o nome do produto: '))
+    price = float(input('Digite o preço do produto: R$ '))
+    soma+=price
+    list.append(price)
+    if price > 1000:
+        cont+=1
+    menor = min(list)
+    if price == menor:
+        barato = nome
+    continua = str(input('Quer continuar? [S/N]: ')).strip().upper() [0]
+    if continua == 'N':
+        break
+print(f'''O total gasto na compra foi de {soma} R$. 
+Ha {cont} produto(s) mais caro(s) que 1000R$. 
+E o produto mais barato custa é o {barato} custando {menor} R$''')
