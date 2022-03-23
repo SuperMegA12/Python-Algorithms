5c23aaa0a06eea00977fe82d2e7dd6fc44b31357
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-23 19:54:41 -0300

    Simulador de Caixa Eletrônico

---
+++ Ex071.py
@@ -0,0 +1,21 @@
+n = int(input('Digite o valor que deseja sacar: R$ '))
+#Apenas inteiros
+total = n
+ced = 50
+totalced = 0
+while True:
+   if total >= ced:
+       total -= ced
+       totalced +=1
+   else:
+       if totalced > 0:
+         print(f'Total de {totalced} cédulas de {ced} R$')
+       if ced == 50:
+         ced = 20
+       elif ced == 20:
+        ced = 10
+       elif ced == 10:
+         ced = 1
+       totalced = 0
+       if total == 0 :
+        break
