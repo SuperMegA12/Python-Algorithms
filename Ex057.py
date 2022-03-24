4cd4068581bb46af1f8bc4f29266cf7ee6cc3345
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 20:44:31 -0300

    Validação de Dados

--- Ex057.py
+++ Ex057.py
@@ -1,3 +1,7 @@
 sexo = str(input('Digite seu sexo [M/F] :')).strip().upper()
 while sexo != 'M' and sexo != 'F':
     sexo = str(input('Dados inválidos. Digite seu sexo [M/F] :')).strip().upper()
+
+
+
+#Ex057 - Validação de Dados
