18d91a6517f68cb96365c68f583ad6062e641fb9
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:54:23 -0300

    Conversor de Temperaturas

--- Ex014.py
+++ Ex014.py
@@ -1,3 +1,7 @@
 celsius = float(input('Digite a temperatura em graus Célsius: C°'))
 conversao = (celsius * 9) / 5 + 32
 print(f'Essa temperatura em Fahrenheit é {conversao} F° ')
+
+
+
+#Ex014 - Conversor de Temperaturas
