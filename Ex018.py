a6aa5baa110c2600374b193fc2aa86d5021bd45e
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 03:01:00 -0300

    Seno, Cosseno e Tangente

--- Ex018.py
+++ Ex018.py
@@ -4,3 +4,7 @@ co = math.cos(math.radians(angulo))
 tan = math.tan(math.radians(angulo))
 seno = math.sin(math.radians(angulo))
 print(f'O seno desse ângulo é {seno:.2f}. A sua tangente é {tan:.2f}. E o seu cosseno é {co:.2f}')
+
+
+
+#Ex018 - Seno, Cosseno e Tangente
