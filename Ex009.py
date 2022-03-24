6e7228f4e4d4ef47c63daf0c6b0b52c6113d2f30
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:49:02 -0300

    Tabuada

--- Ex009.py
+++ Ex009.py
@@ -2,3 +2,7 @@ numero = int(input('Digite um número inteiro para saber sua tabuada:'))
 valor_saber = int(input('Até qual valor você deseja saber a tabuada? :'))
 for cont in range (valor_saber):
     print( f'{numero} X {cont+1} = {numero * (cont+1)}')
+
+
+
+#Ex009 - Tabuada
