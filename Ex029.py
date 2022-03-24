54d345064f0880af287b4d865c98b9d8b8d32f15
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 03:15:04 -0300

    Radar Eletrônico

--- Ex029.py
+++ Ex029.py
@@ -4,3 +4,7 @@ if velocidade > 80:
     print(f'Você ultrapassou o limite de 80KM. Sua multa será de {multa}R$')
 else:
     print('Você está dentro do limite, dirija com cuidado.')
+
+
+
+#Ex029 - Radar Eletrônico
