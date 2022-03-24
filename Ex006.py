33353657f3fef19bd17481a9bd47aa506674696d
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:46:15 -0300

    Dobro, Triplo, Raiz Quadrada

--- Ex006.py
+++ Ex006.py
@@ -1,2 +1,5 @@
 numero = int(input('Digite um número: '))
 print(f'O dobro de {numero} é {numero * 2}, seu triplo é {numero * 3}, e sua raiz quadra é {numero ** 2}')
+
+
+#Ex006 - Dobro, Triplo, Raiz Quadrada
