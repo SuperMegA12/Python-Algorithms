f664e472455a0ec42e60081db600460433110238
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 03:10:27 -0300

    Primeir0 e último nome de uma pessoa

--- Ex027.py
+++ Ex027.py
@@ -1,3 +1,7 @@
 nome = str(input('Digite seu primeiro e segundo nome: ')).strip()
 n = nome.split()
 print(f'Seu primeiro nome é {n[0]} e seu último nome é {n[len(n) - 1]}')
+
+
+
+#Ex027 - Primeiro e último nome de uma pessoa
