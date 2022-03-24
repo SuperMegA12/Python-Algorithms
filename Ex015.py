5c8ea9656c079d92f538eef9e2a0fe9ff6d9ece3
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:56:56 -0300

    Aluguel de Carros

--- Ex015.py
+++ Ex015.py
@@ -4,3 +4,7 @@ custo_km = km_rodados * 0.15
 custo_dias = dias * 60
 print(f'O custo dos dias alugados é de {custo_dias} e de KM rodados é de {custo_km:.2f}')
 print(f'TOTAL A PAGAR {custo_dias + custo_km} R$')
+
+
+
+#Ex015 - Aluguel de Carros
