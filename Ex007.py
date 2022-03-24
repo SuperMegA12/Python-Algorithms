d1fef41841d076b81c656746032e350a06088d4b
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:47:23 -0300

    Média Aritimética

--- Ex007.py
+++ Ex007.py
@@ -2,3 +2,6 @@ nota_1 = float(input('Digite a primeira nota:'))
 nota_2 = float(input('Digite a segunda nota: '))
 media = (nota_1 + nota_2) / 2
 print(f'A média final é {media}')
+
+
+#Ex007 - Média Aritimética
