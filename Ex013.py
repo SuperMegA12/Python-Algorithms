dd892d3c438dab11b1b7c80db54051dc545cfec8
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:53:31 -0300

    Reajuste Salarial

--- Ex013.py
+++ Ex013.py
@@ -1,3 +1,7 @@
 salario = float(input('Qual seu salário? : R$'))
 reajuste = salario + (salario * 15/100)
 print(f'O seu salário de {salario} com o reajuste de 15% a mais é {reajuste}')
+
+
+
+#Ex013 - Reajuste Salarial
