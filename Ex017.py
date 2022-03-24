867e1e871ef5bd9bb65d9953d5e77836fa7883a8
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 03:00:11 -0300

    Catetos e Hipotenusa

--- Ex017.py
+++ Ex017.py
@@ -3,3 +3,7 @@ co = float(input('Digite o cateto oposto:'))
 ca = float(input('Digite o cateto adjacente:'))
 hipot = math.hypot(co,ca)
 print(f'O valor da hipotenusa é {hipot:.2f}')
+
+
+
+#Ex017 - Catetos e Hipotenusa
