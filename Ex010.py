9dee10886226386c62eb95274259d8b873473b48
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 02:49:54 -0300

    Conversor de Moedas

--- Ex010.py
+++ Ex010.py
@@ -1,3 +1,7 @@
 carteira = float(input('Quantos reais você tem na carteira? : R$'))
 pode_comprar = carteira / 6
 print(f'Você pode comprar {pode_comprar:.2f} $')
+
+
+
+#Ex010 - Conversor de Moedas
