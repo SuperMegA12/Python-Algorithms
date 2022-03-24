e33cb01f97be41a7b2a980903bfca2c98bf08f3b
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-24 03:20:23 -0300

    Analisando Triângulos v1.0

--- Ex035.py
+++ Ex035.py
@@ -5,3 +5,7 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Podem formar um triângulo.')
 else:
     print('Não pode formar um triângulo')
+
+
+
+#Ex035 - Analisando Triângulos v1.0
