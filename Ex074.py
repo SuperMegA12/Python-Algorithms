5836aab8a4c5ee03b63a4bb6e44f2725772cfb0b
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-28 17:32:17 -0300

    Maior e Menor valor em Tupla

--- Ex074.py
+++ Ex074.py
@@ -3,7 +3,7 @@ numeros = (randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10)
 
 print(f'Os valores sorteados foram:', end='')
 for n in numeros:
-    print(f'{n} ', end='')
+    print(f' {n} ', end='')
 print(f'\nO maior valor é {max(numeros)}')
 print(f'E o menor é {min(numeros)}')
 
