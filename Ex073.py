ccc24f5836d8be6b811f36e219d05ffb6d416827
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-28 16:40:04 -0300

    Tuplas com Times de Futebol

---
+++ Ex073.py
@@ -0,0 +1,18 @@
+#O código antes da linha 8 é só para uma praticidade de adiconar os nomes.
+tupla = ()
+transformando = list(tupla)
+for c in range(0,8):
+    n = str(input(f'{c+1}ª COLOCADO: ')).strip().upper()
+    transformando.append(n)
+tupla = tuple(transformando)
+
+##############################
+alfabetico = sorted(tupla)
+chape = 'CHAPECOENSE'
+print(f'Os cinco primeiros são {tupla[0:5]} ')
+print(f'Os quatro últimos colocados são {tupla[-4:]}')
+print(f'Os times em ordem alfabética são {alfabetico}')
+if chape in tupla:
+    print(f'O time da Chapecoense está na posição {tupla.index(chape) + 1}ª')
+
+#Ex073 - Tuplas com Times de Futebol
