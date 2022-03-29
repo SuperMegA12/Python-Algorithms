349523d328f4b9ebb5133d6eed00e1bb059bc520
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-29 15:52:03 -0300

    Lista de preços com tupla

---
+++ Ex076.py
@@ -0,0 +1,20 @@
+tupla = ('Lapis.....................R$   1.75',
+         'Borracha..................R$   2.00 ',
+         'Caderno...................R$  15.90',
+         'Estojo....................R$  25.00',
+         'Transferidor..............R$   4.20',
+         'Compasso..................R$   9.99',
+         'Mochila...................R$ 120.32',
+         'Canetas...................R$  22.30',
+         'Livro.....................R$  34.90')
+
+print('-='*25)
+print('               LISTAGEM DE PREÇOS')
+print('-='*25)
+for i in tupla:
+    print(i)
+print('-='*25)
+
+
+
+#Ex076 - Lista de preços com tupla
