a18cb9ee61ff9c8f5515541f5f1b1ea5295ac1a6
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-03-31 21:08:21 -0300

    Validando Expressões Matemáticas

---
+++ Ex083.py
@@ -0,0 +1,16 @@
+abreParent = []
+
+expressao = input('Digite uma expressão: ').strip().lower()
+
+for caracter in expressao:
+    if caracter == '(':
+        abreParent.append(caracter)
+
+    if caracter == ')':
+        abreParent.pop() if len(abreParent) > 0 else abreParent.append(')')
+
+print('Expressão Correta...' if len(abreParent) == 0 else 'Expressão incorreta...')
+
+
+
+#Ex083 - Validando Expressões Matemáticas
