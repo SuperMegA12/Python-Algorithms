991e3ed37c3cce0960718892ebebae5e6f56ae05
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-04-05 15:38:38 -0300

    Lista Ordenada sem repetições

--- Ex080.py
+++ Ex080.py
@@ -1,1 +1,14 @@
+lista = []
+for c in range(0, 5):
+    n = int(input("Digite um valor: "))
+    for b in range(0, len(lista)):
+        if n <= lista[b]:
+            lista.insert(b, n)
+            break
+    if n not in lista:
+        lista.append(n)
+print(lista)
 
+
+
+#Ex080 - Lista Ordenada sem repetições
