754229233f46e23555290d860399a9c1bc1684ef
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-04-14 20:20:00 -0300

    Dicionário em Python

---
+++ Ex090.py
@@ -0,0 +1,14 @@
+dicionario = {}
+dicionario["Nome"] = str(input('NOME:'))
+dicionario["Média"] = float(input('Média:'))
+print(f'O nome é: {dicionario["Nome"]}')
+print(f'A sua média é: {dicionario["Média"]}')
+if dicionario["Média"] < 7:
+    print('Reprovado!')
+else:
+    print('Aprovado!')
+
+
+
+#Ex090 - Dicionário em Python
+
