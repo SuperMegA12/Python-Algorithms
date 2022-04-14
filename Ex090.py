6c0e70ec4fa9c92a80b478f0d0cebe45cc362194
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-04-14 20:25:21 -0300

    Dicionário em Python

--- Ex090.py
+++ Ex090.py
@@ -3,10 +3,12 @@ dicionario["Nome"] = str(input('NOME: '))
 dicionario["Média"] = float(input('Média: '))
 print(f'O nome é: {dicionario["Nome"]}')
 print(f'A sua média é: {dicionario["Média"]}')
-if dicionario["Média"] < 7:
+if dicionario["Média"]  < 5:
     print('Reprovado!')
+elif dicionario["Média"] >= 5 and dicionario["Média"] < 7:
+    print('Recuperação!')
 else:
-    print('Aprovado!')
+    print("Aprovado!!!")
 
 
 
