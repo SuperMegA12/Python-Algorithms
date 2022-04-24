224c09032d0540a2e504d269ecfa2da7316feef4
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-04-24 11:18:16 -0300

    Função que cálcula área

---
+++ Ex96.py
@@ -0,0 +1,18 @@
+def area():
+    print('Controle de terreno')
+    print('-'*30)
+    a =float(input('Largura (m): '))
+    b = float(input('Comprimento (m): '))
+    print(f'A área de um terreno {a}x{b} é de {a*b}m²')
+
+
+
+
+
+
+
+area()
+
+
+
+#Ex096 - Função que cálcula área
