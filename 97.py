b1b53fde2d4a5ba2abdecaab3df64cd5093316ae
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-04-24 11:33:20 -0300

    Um print especial

---
+++ 97.py
@@ -0,0 +1,18 @@
+def escreva(msg):
+    tamanho = len(msg) + 4
+    print('~'*tamanho)
+    print(f'  {msg}')
+    print('~'*tamanho)
+
+
+
+
+
+
+
+escreva('Olá mundo')
+escreva('Curso em vídeo youtube')
+escreva('DEV')
+
+
+#Ex97 - Um print especial
