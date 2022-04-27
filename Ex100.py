7494fcda25bde53c16f2736fec3f7787ffcb612a
SuperMEgA12 <83327821+SuperMegA12@users.noreply.github.com>
2022-04-27 15:14:54 -0300

    Função para sortear e somar

---
+++ Ex100.py
@@ -0,0 +1,22 @@
+from random import randint
+
+lista = []
+
+
+def sorteia(*n):
+   print('Os 5 números sorteados foram: ', end='')
+   soma = 0
+   for i in n:
+       print(f'{i} ', end='')
+       if i % 2 == 0:
+           soma+=i
+   print(f'\nSomando os valores pares, o resultado é: {soma}')
+
+
+
+
+sorteia(randint(0,10), randint(0,10), randint(0,10),randint(0,10),randint(0,10))
+
+
+
+#Ex100 - Função para sortear e somar
